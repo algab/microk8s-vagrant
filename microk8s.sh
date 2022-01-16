f8bb4a6fe3a32cca6319a0d9829060c39093e083
Alvaro Oliveira <algab.oliveira@gmail.com>
2022-01-16 12:27:55 -0300

    add ip hosts shell

--- microk8s.sh
+++ microk8s.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo echo 192.168.0.10 server01 >> /etc/hosts
+sudo echo 192.168.0.11 server02 >> /etc/hosts
 sudo snap install microk8s --classic --channel=1.22/stable
 sudo usermod -a -G microk8s vagrant
 sudo chown -f -R vagrant ~/.kube
